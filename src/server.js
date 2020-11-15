eb1f5bda865309478146e2303c03824eb38ebc1d
Sean <sean.lorman@gmail.com>
2020-11-14 18:11:50 -0700

    Add basic server file

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pgdb = require("./db/db");
+const bp = require('body-parser');
+const amqp = require('amqplib/callback_api');
+
+const server = express();
+const port = 3000;
+
+// Middleware
+server.use(bp.json());
+
+// GET Routes
+
+server.get('/', (__, res) => {
+    pgdb.getAllBadges().then((results) => res.send(results))
+});
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
